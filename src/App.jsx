7fd0bb1a9040a3cf37a3564e428be8f15624b6df
anitapanczel <anita@panczel.hu>
2022-02-03 10:20:12 +0100

    up

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,8 @@ function App() {
   const [title, setTitle] = useState("");
   const [content, setContent] = useState("");
   
-  /* Add new blogpost */
-
+  /* ***ADD NEW BLOGPOST*** */
+  
   const addBlogPost = () => {
     setBlogPosts([
       ...blogPosts,
@@ -16,13 +16,77 @@ function App() {
     setTitle("");
     setContent("");
   }
-
-  /* Delete all blogpost */
-
+  
+  /* ***DELETE ALL BLOGPOST*** */
+  
   const deleteBlogPosts = () => {
     setBlogPosts([])
   }
- 
+  
+  /* ***REMOVE A POST*** */
+  
+  const removeBlogPost = (date) => {
+    
+    /* EZ HELYETT CSINÁLTUK A FILTERT
+    
+    const newList = [];
+    for (const post of blogPosts) {
+      if (post.date !== date) {
+        newList.push(post);
+      }
+    }
+    setBlogPosts(newList);
+    */
+   
+   setBlogPosts(blogPosts.filter((post) => post.date !== date));
+  }
+  
+  /* ***EDIT POST*** */
+  
+  const editBlogPost = (date, editTitle, editContent) => {
+    const currentList = blogPosts;
+    const nextList = [];
+    for (const post of currentList) {
+      if (post.date !== date) {
+        nextList.push(post);
+      } else {
+        nextList.push(
+          {title: editTitle, content: editContent, date: post.date }
+          );
+        }
+      }
+      setBlogPosts(nextList);
+    }
+    
+  /* ÚJ ARTICLE KOMPONENS */
+  
+    const BlogPost = ({blogPost, editBlogPost, removeBlogPost}) => {
+
+      const [editTitle, setEditTitle] = useState("");
+      const [editContent, setEditContent] = useState("");
+      
+      return (
+        <article>
+        <h1>{blogPost.title}</h1>
+        <div>{blogPost.content}</div>
+        <h5>{blogPost.date}</h5>
+        <input type="text" placeholder="Title" value={editTitle} onChange={(event) => setEditTitle(event.target.value)}/>
+        <input type="text" placeholder="Content" value={editContent} onChange={(event) => setEditContent(event.target.value)} />
+        <button onClick={() => {
+          editBlogPost(blogPost.date, editTitle, editContent);
+          setEditTitle("");
+          setEditContent("");
+        }}
+        >Edit</button>
+        <button>Save</button>
+        <button>Cancel</button>
+        <button onClick={() => removeBlogPost(blogPost.date)}>Remove</button>
+      </article>
+    );
+
+  };
+
+
   return (
     <main className="container">
 
@@ -34,17 +98,7 @@ function App() {
 
       <h1>Posztok</h1>
       {blogPosts.map((blogPost, index) => (
-        <article key={index}>
-          <h1>{blogPost.title}</h1>
-          <div>{blogPost.content}</div>
-          <h5>{blogPost.date}</h5>
-          <input type="text" placeholder="Title"/>
-          <input type="text" placeholder="Content" />
-          <button>Edit</button>
-          <button>Save</button>
-          <button>Cancel</button>
-          <button>Remove</button>
-        </article>
+        <BlogPost key={index} blogPost={blogPost} editBlogPost={editBlogPost} removeBlogPost={removeBlogPost} />
       ))}
        <button onClick={deleteBlogPosts}>Delete all posts</button>
     </main>
